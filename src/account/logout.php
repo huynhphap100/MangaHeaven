a217fac88a4eca74496f4af270c1938de25b8279
Sitonai <huynhphap100@gmail.com>
2023-10-23 00:58:55 +0700

    first commit

--- src/account/logout.php
+++
@@ -1,5 +0,0 @@
-<?php 
-	unset($_SESSION['user']);
-	$user = null;
-	header('Location: ../WebDocTruyen');
-?>
--- src/footer.php
+++
@@ -1,6 +0,0 @@
-<div class="w3-cell-row w3-border-top w3-border-white w3-padding">
-	<a href="#" class="w3-cell" style="width: 20%;">Điều khoản</a>
-	<a href="#" class="w3-cell" style="width: 20%;">Chính sách bảo mật</a>
-	<p class="w3-cell" style="width: 25%;">Copyright © 2023 MangaHeaven</p>
-	<p class="w3-cell" style="width: 20%;">Email: <a href="#">sitonai.p@gmail.com</a></p>
-</div>
